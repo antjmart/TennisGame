04d5fdc72db04f7560ea495d79107945a97f52b3
Anthony Martinez <ajmarti8@uci.edu>
2024-09-21 20:55:28 -0700

    Implemented constructor for Game class.

--- tennis.cpp
+++ tennis.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include "tennis.h"
 
+Game::Game(bool player_is_serving)
+    : player_score(0), opponent_score(0), player_serve(player_is_serving)
+{}
+
+void Game::update_score() {
+    return;
+}
+
+int Game::verify_winner() const {
+    return 0;
+}
