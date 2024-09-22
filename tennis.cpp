7b536da43434219372bf5ca162a27bce0bd7cc1d
Anthony Martinez <ajmarti8@uci.edu>
2024-09-21 21:19:36 -0700

    Simple implementation for Game class play_point() method, for now. Command line input will be used to determine point winner.

--- tennis.cpp
+++ tennis.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include "tennis.h"
+using namespace std;
+
 
 Game::Game(bool player_is_serving)
     : player_score(0), opponent_score(0), player_serve(player_is_serving)
 {}
+
+int Game::play_point() const {
+    cout << "Point Win: ";
+    char c;
+    cin >> c;
+
+    if (c == 'y')
+        return 1;
+    else
+        return 2;
+}
 
 void Game::update_score() {
     return;
