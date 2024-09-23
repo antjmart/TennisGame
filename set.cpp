8b127c269f5e840d3aa2280d419b289d6c6a6203
Anthony Martinez <ajmarti8@uci.edu>
2024-09-23 01:49:09 -0700

    Printing for Set class implemented

--- set.cpp
+++ set.cpp
@@ -6,3 +6,7 @@ Set::Set(bool player_is_serving)
     : player_games(0), opponent_games(0), player_serve(player_is_serving)
 {}
 
+ostream & operator << (ostream & out, const Set & s) {
+    out << s.player_games << " - " << s.opponent_games;
+    return out;
+}
