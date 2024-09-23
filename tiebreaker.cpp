6ae13b35131c0d1233ee745cd8d7b63781d1e95d
Anthony Martinez <ajmarti8@uci.edu>
2024-09-22 23:56:33 -0700

    Implemented constructor for Tiebreaker class

--- tiebreaker.cpp
+++ tiebreaker.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include "tennis.h"
 using namespace std;
+
+Tiebreaker::Tiebreaker(bool player_is_serving)
+    : Game(player_is_serving)
+{}
+
+void Tiebreaker::update_score(bool won_point) {
+    return;
+}
+
+int Tiebreaker::verify_winner() const {
+    return 0;
+}
