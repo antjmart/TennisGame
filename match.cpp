9ed9b05dd6b41109c34fed6b7d4250751d9bacc7
Anthony Martinez <ajmarti8@uci.edu>
2024-09-30 13:53:15 -0700

    Implemented Match class copy constructor

--- match.cpp
+++ match.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
 #include "tennis.h"
 using namespace std;
 
 Match::Match(bool five_set_match)
     : sets_won(0), sets_lost(0), five_setter(five_set_match)
+{}
+
+Match::Match(const Match & m)
+    : sets(m.sets), sets_won(m.sets_won), sets_lost(m.sets_lost), five_setter(m.five_setter)
 {}
