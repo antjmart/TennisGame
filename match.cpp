f79fbe29c0970ff485d5b348f9e7adb631a05aec
Anthony Martinez <ajmarti8@uci.edu>
2024-09-30 15:19:54 -0700

    Implemented Match class play_set() method

--- match.cpp
+++ match.cpp
@@ -24,3 +24,28 @@ ostream & operator << (ostream & out, const Match & m) {
     out << endl;
     return out;
 }
+
+int Match::play_set() {
+    cout << "Starting Set " << (sets.size() + 1) << "..." << endl << endl;
+    bool player_will_serve;
+    if (sets.size() > 0)
+        player_will_serve = sets.back().player_serve;
+    else {
+        player_will_serve = rand() % 2 == 0;
+        cout << "Start of match, so serve will be chosen randomly..." << endl;
+    }
+    cout << "You will " << (player_will_serve ? "serve" : "receive") << '.' << endl << endl;
+
+    Set set(player_will_serve);
+    int set_winner = 0;
+    cout << "Set Score: 0 - 0" << endl << endl;
+
+    while (set_winner == 0) {
+        set.update_games(set.play_game());
+        set_winner = set.verify_winner();
+        cout << "Set Score: " << set << endl << endl;
+    }
+
+    sets.push_back(set);
+    return set_winner == 1;
+}
